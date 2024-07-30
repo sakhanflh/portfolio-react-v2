7008a2358ee078ac6c65719d95baec996066bd03
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-07-30 17:02:08 +0700

    Navbar: add useLocation

--- src/components/fragments/Navbar.jsx
+++ src/components/fragments/Navbar.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { HoverIcon } from "../../assets/HoverIcon";
 
 export function Navbar() {
+    const location = useLocation();
     return (
         <>
             <div className="max-w-[150rem] overflow-x-hidden">
                 <div className="hidden w-full h-16 px-[5%] lg:flex items-center justify-between font-jost">
                     <div className="flex w-12">
-                        {/* <img src="/img/Senech-white.png" alt="" className="w-full h-full object-cover" /> */}
                         <HoverIcon
                             src="/img/Senech-white.png"
                             hoverSrc="/img/senech-blue.png"
@@ -16,21 +16,41 @@ export function Navbar() {
                     </div>
 
                     <ul className="flex gap-10">
-                        <Link to={'/'}>
-                            <li className="text-text text-sm hover:text-hover font-semibold">Home</li>
-                        </Link>
-                        <Link to={'/aboutme'}>
-                            <li className="text-text text-sm hover:text-hover font-semibold">About Me</li>
-                        </Link>
-                        <Link to={'/projects'}>
-                            <li className="text-text text-sm hover:text-hover font-semibold">Projects</li>
-                        </Link>
-                        <Link to={'/certificates'}>
-                            <li className="text-text text-sm hover:text-hover font-semibold">Certificate</li>
-                        </Link>
+                        <li>
+                            <Link
+                                to="/"
+                                className={`text-text text-sm hover:text-hover font-semibold ${location.pathname === '/' ? 'text-span' : ''}`}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/aboutme"
+                                className={`text-text text-sm hover:text-hover font-semibold ${location.pathname === '/aboutme' ? 'text-span' : ''}`}
+                            >
+                                About Me
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/projects"
+                                className={`text-text text-sm hover:text-hover font-semibold ${location.pathname === '/projects' ? 'text-span' : ''}`}
+                            >
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/certificates"
+                                className={`text-text text-sm hover:text-hover font-semibold ${location.pathname === '/certificates' ? 'text-span' : ''}`}
+                            >
+                                Certificate
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
         </>
-    )
+    );
 }
